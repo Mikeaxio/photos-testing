895d0c3bd6eaf4e8eca6f28e1f896536dfd09f3a
Michael Axiotakis <maxiotakis@newburyresidential.com>
2023-11-12 18:48:34 -0500

    other

--- src/app/Cover.js
+++ src/app/Cover.js
@@ -2,18 +2,21 @@ import Image from "next/image";
 
 export default function Cover() {
   return (
-    <Image
-    src={
-      "https://newburypublic.s3.amazonaws.com/website/page-contact-cover.webp"
-    }
-    fill
-    sizes="100vw"
-    quality={100}
-    alt="Apartments Cover Image"
-    style={{ objectFit: "cover" }}
-    priority={true}
-    placeholder="blur"
-    blurDataURL={"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAADCAIAAAA7ljmRAAAACXBIWXMAAAPoAAAD6AG1e1JrAAAAMklEQVR4nAEnANj/AK+TgjQ0Nay9zmxvfAB8Y1qdmZ9MRkwAAAUA//j46tHLx8rS5ubmWkEUa1u5LjkAAAAASUVORK5CYII="}
-  />
+    <div>
+    {/* <Image
+//     src={
+//       "https://newburypublic.s3.amazonaws.com/website/page-contact-cover.webp"
+//     }
+//     fill
+//     sizes="100vw"
+//     quality={100}
+//     alt="Apartments Cover Image"
+//     style={{ objectFit: "cover" }}
+//     priority={true}
+//     placeholder="blur"
+//     blurDataURL={"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAADCAIAAAA7ljmRAAAACXBIWXMAAAPoAAAD6AG1e1JrAAAAMklEQVR4nAEnANj/AK+TgjQ0Nay9zmxvfAB8Y1qdmZ9MRkwAAAUA//j46tHLx8rS5ubmWkEUa1u5LjkAAAAASUVORK5CYII="}
+   /> */}
+hi
+</div>
   )
 }
